dc56eeb267559f3b5df176b0fb5da3b5c1ac08ad
Yasin Junet <hello@yasinjunet.com>
2025-06-03 19:20:19 +0700

    fix: language switcher redirect to current path

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,6 +1,21 @@
 import { locales, defaultLocale } from '@/configs';
 
-const LanguageSwitcher = ({ currentLanguage }: { currentLanguage: string }) => {
+const stripLocalePrefix = (path: string) => {
+  const [, first] = path.split('/');
+  return locales.some((l) => l.value === first)
+    ? path.split('/').slice(2).join('/')
+    : path.slice(1);
+};
+
+const LanguageSwitcher = ({
+  currentLanguage,
+  currentPath,
+}: {
+  currentLanguage: string;
+  currentPath: string;
+}) => {
+  const normalizedPath = stripLocalePrefix(currentPath);
+
   return (
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost rounded-field">
@@ -9,7 +24,13 @@ const LanguageSwitcher = ({ currentLanguage }: { currentLanguage: string }) => {
       <ul className="menu dropdown-content bg-base-200 text-neutral rounded-box z-1 mt-4 w-52 p-2 shadow-sm">
         {locales.map((locale) => (
           <li key={locale.value}>
-            <a href={`/${locale.value === defaultLocale ? '' : locale.value}`}>
+            <a
+              href={
+                locale.value === defaultLocale
+                  ? `/${normalizedPath}`
+                  : `/${locale.value}/${normalizedPath}`
+              }
+            >
               {locale.label}
             </a>
           </li>
