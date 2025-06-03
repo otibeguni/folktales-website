150fc19bb261a52ceff23efea28ba070b0fc5690
Yasin Junet <hello@yasinjunet.com>
2025-06-03 23:51:35 +0700

    fix: link on story-card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,12 @@
 import { type IStory } from '@/types';
 
-const Card = ({ title, category, slug }: IStory) => {
+const Card = ({ title, category, slug, language }: IStory) => {
+  const basePath = language === 'en' ? '' : `/${language}`;
+
   return (
     <div className="card bg-base-100 card-md shadow-sm">
       <div className="card-body">
-        <a href={`/stories/${slug}`} className="card-title">
+        <a href={`${basePath}/stories/${slug}`} className="card-title">
           {title}
         </a>
         <p>{category}</p>
