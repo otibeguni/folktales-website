2904979b728be2f53f1c23ee3c50216b99ca67e8
Yasin Junet <hello@yasinjunet.com>
2025-09-01 10:45:33 +0700

    style: adjust image cover size of card-story component

--- src/components/CardStory.tsx
+++ src/components/CardStory.tsx
@@ -14,16 +14,16 @@ const CardStory = ({
     <a
       key={`${frontmatter.language}-${frontmatter.slug}`}
       href={`${basePath}/${path}/${frontmatter.slug}`}
-      className="bg-base-100 hover:bg-base-200/50 flex flex-col rounded-lg no-underline shadow-sm transition-all duration-300 hover:shadow-md"
+      className="bg-base-100 hover:bg-base-200/50 flex flex-col rounded-lg no-underline shadow-sm transition-all duration-300 hover:shadow-md p-4"
     >
-      <div className="aspect-square overflow-hidden">
+      <div className="mx-auto">
         <img
           src={`https://placehold.co/360x360/f0f0f0/333333?text=${frontmatter.title}`}
           alt={frontmatter.title}
-          className="bg-base-300 object-cover w-full h-full"
+          className="bg-base-300 aspect-square w-[256px] rounded-lg"
         />
       </div>
-      <div className="p-4 space-y-1">
+      <div className="space-y-1 pt-4">
         <div className="text-left">
           <div className="text-base-content text-lg font-bold">
             {frontmatter.title}
