9567c799ed26a028de2e823834cd498af16c13d1
Shahriar Haque <shahriar.haque@gmail.com>
2025-12-29 00:48:48 +1100

    chore: updated blog illustrations

--- src/components/CardBodyContent.tsx
+++ src/components/CardBodyContent.tsx
@@ -16,15 +16,17 @@ const CardBodyContent = ({
     <div className="prose article-content mt-8 max-w-none flex-1">
       <a
         href={`/${path}/${frontmatter.slug}`}
-        className="bg-base-100 flex flex-col rounded-lg no-underline"
+        className="bg-base-100 flex flex-col gap-4 rounded-xl no-underline"
       >
         {coverImage && (
-          <img
-            src={coverImage}
-            alt={frontmatter.title}
-            className="bg-base-300 aspect-square w-3xs rounded-lg"
-            loading="lazy"
-          />
+          <div className="overflow-hidden rounded-xl">
+            <img
+              src={coverImage}
+              alt={frontmatter.title}
+              className="aspect-square w-full object-cover block"
+              loading="lazy"
+            />
+          </div>
         )}
         <h1 className="mb-4 text-5xl leading-16">{frontmatter?.title}</h1>
         <span className="badge badge-soft badge-primary">
